89acba252cc95d320db063659ef0f13bd74e73b7
Hannah Saeed <hannahsaeed@Hannahs-MacBook-Pro.local>
2025-09-14 11:05:19 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 void enterArrayData(double* array);
 void outputArrayData(double* array);
-double sumArray(double* dynamic);
+double sumArray(double* array);
 int main()
 {
     
+}
+void enterArrayData(double* array)
+{
+    cout << "Data entry for the array:" << endl;
+    for(int i = 0; i < 5; i++)
+    {
+        cout << "> Element #" << i << ": " << endl;
+        cin >> array[i];
+    }
 }
